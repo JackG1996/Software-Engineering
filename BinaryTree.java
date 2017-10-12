9686aa33344b625a0b8aeb9e3c2bfcf349356dce
Jack Gallagher <gallaj10@tcd.ie>
2017-10-12 20:20:59 +0100

    Working BST with LCA and some tests

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,61 +1,99 @@
+import java.util.NoSuchElementException;
 
+public class BinaryTree<Key extends Comparable<Key>, Value> {
+	private Node root;
 
-public class BinaryTree 
-{
-	Node root;
+	private class Node {
 
-	Node lca(Node node, int n1, int n2) 
-	{
-		if(node == null)
-		{
+		private Key key; // sorted by key
+		private Value val; // associated data
+		private Node left, right; // left and right subtrees
+		private int N; // number of nodes in subtree
+
+		public Node(Key key, Value val, int N) {
+
+			this.key = key;
+			this.val = val;
+			this.N = N;
+		}
+	}
+
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	private int size(Node x) {
+		if (x == null)
+			return 0;
+		else
+			return x.N;
+	}
+
+	public boolean contains(Key key) {
+		return get(key) != null;
+	}
+
+	public Value get(Key key) {
+		return get(root, key);
+	}
+
+	private Value get(Node x, Key key) {
+		if (x == null)
+			return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			return get(x.left, key);
+		else if (cmp > 0)
+			return get(x.right, key);
+		else
+			return x.val;
+
+	}
+
+	public void put(Key key, Value val) {
+		root = put(root, key, val);
+	}
+
+	private Node put(Node x, Key key, Value val) {
+		if (x == null)
+			return new Node(key, val, 1);
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			x.left = put(x.left, key, val);
+		else if (cmp > 0)
+			x.right = put(x.right, key, val);
+		else
+			x.val = val;
+		x.N = 1 + size(x.left) + size(x.right);
+		return x;
+	}
+
+	public String lca(Key x1, Key x2) {
+		if (!(contains(x1) && contains(x2))) {
 			return null;
 		}
-
-		else if(node.data < n1 && node.data < n2)
-		{
-			node = node.right;
-			return lca(node,n1,n2);
+		if (root == null) {
+			return null;
 		}
-		else if(node.data > n1 && node.data > n2)
-		{
+		return lca(root, x1, x2);
+	}
+
+	private String lca(Node node, Key x1, Key x2) {
+
+		if (x1.compareTo(node.key) < 0 && x2.compareTo(node.key) < 0) {
 			node = node.left;
-			return lca(node,n1,n2); // return and no return diff answers
+			return lca(node, x1, x2);
+		} else if (x1.compareTo(node.key) > 0 && x2.compareTo(node.key) > 0) {
+			node = node.right;
+			return lca(node, x1, x2);
 		}
 
-		return node;
+		return String.valueOf(node.key);
 	}
-  
 
-	public static void main(String args[]) 
-	{
-		// Let us construct the BST shown in the above figure
-		BinaryTree tree = new BinaryTree();
-		tree.root = new Node(20);
-		tree.root.left = new Node(8);
-		tree.root.right = new Node(22);
-		tree.root.left.left = new Node(4);
-		tree.root.left.right = new Node(12);
-		tree.root.left.right.left = new Node(10);
-		tree.root.left.right.right = new Node(14);
-		
-		int n1=4 , n2 =12;
-		Node t = tree.lca(tree.root, n1, n2);
-		System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
-		
-		n1 = 10; n2 = 14;
-		t = tree.lca(tree.root, n1, n2);
-		System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
-
-		n1 = 14;
-		n2 = 8;
-		t = tree.lca(tree.root, n1, n2);
-		System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
-
-		n1 = 10;
-		n2 = 22;
-		t = tree.lca(tree.root, n1, n2);
-		System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
-
-	}
 }
 
