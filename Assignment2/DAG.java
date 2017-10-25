7be2ef5f1c360fcd98cd86e29b7b01326eb1f822
Jack Gallagher <gallaj10@tcd.ie>
2017-10-25 23:44:08 +0100

    Beginning creations

---
+++ Assignment2/DAG.java
@@ -0,0 +1,25 @@
+
+// A Java program to print topological sorting of a DAG
+import java.io.*;
+import java.util.*;
+
+// This class represents a directed graph using adjacency
+// list representation
+public class DAG {
+
+	private int V; // No. of vertices
+	private LinkedList<Integer> adj[]; // Adjacency List
+
+	// Constructor
+	DAG(int v) {
+		V = v;
+		adj = new LinkedList[v];
+		for (int i = 0; i < v; ++i)
+			adj[i] = new LinkedList();
+	}
+
+	// Function to add an edge into the graph
+	void addEdge(int v, int w) {
+		adj[v].add(w);
+	}
+}
